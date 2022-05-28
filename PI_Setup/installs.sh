dea7e2075dc7ae9e85ed5ca241540886528b2cf9
Lukas Mueller <Mueller.Lukas@outlook.com>
2022-05-28 12:41:35 +0200

    edited install script

--- PI_Setup/installs.sh
+++ PI_Setup/installs.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 #this script installs all external packages that were used
 #the used PI was a 3+ model running Ubiquity ROSberryPi
 #on Ubuntu 20.04
 
+#run chmod +x installs.sh to make this script executable
