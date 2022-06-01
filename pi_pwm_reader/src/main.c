4785d2d8a0a1ccc984f6cd5b820197739f2d2d9f
Lukas Mueller <Mueller.Lukas@outlook.com>
2022-06-02 00:10:24 +0200

    testing first interrupt

--- pi_pwm_reader/src/main.c
+++ pi_pwm_reader/src/main.c
@@ -1,16 +1,29 @@
 #include <wiringPi.h>
 #include <stdio.h>
 
+void testInterrupt(void);
+
 int main (void)
 {
   int iPinStatus = 0;
   wiringPiSetup () ;
   pinMode (25, INPUT) ;
-  for (;;)
-  {
-    iPinStatus = digitalRead(25);
-    printf("%d \n",iPinStatus);
+
+  wiringPiISR (25, INT_EDGE_RISING,  testInterrupt); 
+  // for (;;)
+  // {
+  //   iPinStatus = digitalRead(25);
+  //   print("%d \n",iPinStatus);
+
+  // }
+  for(;;){
 
   }
+
+
   return 0 ;
+}
+
+void testInterrupt(void){
+  printf("Interrupt \n");
 }
